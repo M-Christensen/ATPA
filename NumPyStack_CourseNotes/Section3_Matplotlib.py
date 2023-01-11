1e1b89cbe6259e77f3514bb4b204135a9c255469
McKay Christensen <mckayc.647@gmail.com>
2023-01-11 07:04:14 -0500

    Small changes

---
+++ NumPyStack_CourseNotes/Section3_Matplotlib.py
@@ -0,0 +1,10 @@
+#### Code for Section 3 - Matplotlib of the Deep Learning Prerequisities course
+# Plotting in VSCode .py files: https://donjayamanne.github.io/pythonVSCodeDocs/docs/jupyter_examples/
+
+' Lesson 14. Line Chart'
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Line
+
